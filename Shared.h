0b63e95478b3d8b987934ca8c32fa2217fcac380
Mark Fickett <mark.fickett@gmail.com>
2012-02-05 21:20:59 -0500

    Set default baud rate rate to 38400, list other Arduino baud rates.

--- Shared.h
+++ Shared.h
@@ -4,7 +4,15 @@
  * Values shared with the Python DataSender.
  */
 
-#define SERIAL_BAUD 28800
+// define SERIAL_BAUD 4800
+// define SERIAL_BAUD 9600
+// define SERIAL_BAUD 14400
+// define SERIAL_BAUD 19200
+// define SERIAL_BAUD 28800
+#define SERIAL_BAUD 38400
+// define SERIAL_BAUD 57600
+// define SERIAL_BAUD 115200
+
 #define READY_STRING "Ready."
 #define NUMERIC_BYTE_LIMIT 255
 #define END_OF_KEY 0
